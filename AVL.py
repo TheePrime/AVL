62010347987ac2a4ddcf3830e857a28693e77ac0
Abel Misiocha <wells.mail.13@gmail.com>
2025-01-08 17:51:14 +0300

    feat(core): Add delete funcs to avl

--- AVL.py
+++ AVL.py
@@ -36,6 +36,43 @@ class AVLTree:
 
         return root
 
+    #Delete from tree
+    def delete(self, root, key):
+        if not root:
+            return root
+        elif key < root.key:
+            root.left = self.delete(root.left, key)
+        elif key > root.key:
+            root.right = self.delete(root.right, key)
+        else:
+            if root.left is None:
+                return root.right
+            elif root.right is None:
+                return root.left
+            temp = self.get_min_value_node(root.right)
+            root.key = temp.key
+            root.price = temp.price
+            root.right = self.delete(root.right, temp.key)
+
+        if root is None:
+            return root
+
+        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
+        balance = self.get_balance(root)
+
+        if balance > 1 and self.get_balance(root.left) >= 0:
+            return self.right_rotate(root)
+        if balance > 1 and self.get_balance(root.left) < 0:
+            root.left = self.left_rotate(root.left)
+            return self.right_rotate(root)
+        if balance < -1 and self.get_balance(root.right) <= 0:
+            return self.left_rotate(root)
+        if balance < -1 and self.get_balance(root.right) > 0:
+            root.right = self.right_rotate(root.right)
+            return self.left_rotate(root)
+
+        return root
+
     #Get Height
     #Get the height of the three from the root Node
     def get_height(self, root):
