9719a948256dc5c227b4bb482180de7041b51753
Abdalah Said <144815131+fozzah@users.noreply.github.com>
2025-01-08 16:08:44 +0300

    Updated and tested the implementation of insertion to cart

--- AVL.py
+++ AVL.py
@@ -1,3 +1,4 @@
+#AVL node for use in the AVL tree
 class AVLNode:
     def __init__(self, key, price):
         self.key = key
@@ -5,3 +6,32 @@ class AVLNode:
         self.height = 1
         self.left = None
         self.right = None
+
+
+class AVLTree:
+    def __init__(self):
+        self.root = None
+    #Insert to the Tree while checking the balance factor
+    def insert(self, root, key, price):
+        if not root:
+            return AVLNode(key, price)
+        elif key < root.key:
+            root.left = self.insert(root.left, key, price)
+        else:
+            root.right = self.insert(root.right, key, price)
+
+        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
+        balance = self.get_balance(root)
+
+        if balance > 1 and key < root.left.key:
+            return self.right_rotate(root)
+        if balance < -1 and key > root.right.key:
+            return self.left_rotate(root)
+        if balance > 1 and key > root.left.key:
+            root.left = self.left_rotate(root.left)
+            return self.right_rotate(root)
+        if balance < -1 and key < root.right.key:
+            root.right = self.right_rotate(root.right)
+            return self.left_rotate(root)
+
+        return root
---
+++ main.py
@@ -0,0 +1,7 @@
+from Cart import Cart
+
+#initialize the cart
+cart = Cart()
+# Add item to cart
+cart.add_item('apple', 1.5)
+
